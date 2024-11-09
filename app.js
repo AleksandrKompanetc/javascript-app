46854088715c25f974a84888e8c3f37d6ac7c818
AleksandrKompanetc <moneymacker7@gmail.com>
2024-11-09 23:54:34 +0200

    changing app file

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const resultElement = document.getElementById('result');
 const input1 = document.getElementById('input1');
 const input2 = document.getElementById('input2');
+const submitBtn = document.getElementById('submit');
 
 const sum = Number(input1.value) + Number(input2.value);
 resultElement.textContent = sum;
-console.log(typeof sum);
+// console.log(typeof sum);
+
+submitBtn.onclick = function() {
+  const sum = Number(input1.value) + Number(input2.value);
+  resultElement.textContent = sum;
+}
