2620ebfd0e0d0a592d4494ede310b24cfc1f680d
AleksandrKompanetc <moneymacker7@gmail.com>
2024-11-10 19:42:47 +0200

    creating calculator

--- app.js
+++ app.js
@@ -2,12 +2,39 @@ const resultElement = document.getElementById('result');
 const input1 = document.getElementById('input1');
 const input2 = document.getElementById('input2');
 const submitBtn = document.getElementById('submit');
+const plusBtn = document.getElementById('plus');
+const minusBtn = document.getElementById('minus');
+let action = '+';
 
 const sum = Number(input1.value) + Number(input2.value);
 resultElement.textContent = sum;
 // console.log(typeof sum);
 
+plusBtn.onclick = function() {
+  action = '+'
+}
+
+minusBtn.onclick = function() {
+  action = '-'
+}
+
+
 submitBtn.onclick = function() {
-  const sum = Number(input1.value) + Number(input2.value);
-  resultElement.textContent = sum;
+  if (action == '+') {
+    const sum = Number(input1.value) + Number(input2.value);
+    if (sum < 0) {
+      resultElement.style.color = 'red'
+    } else {
+      resultElement.style.color = 'green'
+    }
+    resultElement.textContent = sum;
+  } else if (action == '-') {
+    const sum = Number(input1.value) - Number(input2.value);
+    if (sum < 0) {
+      resultElement.style.color = 'red'
+    } else {
+      resultElement.style.color = 'green'
+    }
+    resultElement.textContent = sum;
+  }
 }
