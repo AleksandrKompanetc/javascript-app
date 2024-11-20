b3022e416a75c992f5a77970ab7a96ce8610fccf
AleksandrKompanetc <moneymacker7@gmail.com>
2024-11-20 22:43:18 +0200

    adding async promise

--- app.js
+++ app.js
@@ -561,24 +561,32 @@ const person = {
 //   console.log('timeout')
 // }, 2000)
 
-const delay = (time = 1000) => {
-  const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      // resolve([1, 2, 3])
-      reject('Error in delay')
-    }, time)
-  })
-  return promise
-}
+// const delay = (time = 1000) => {
+//   const promise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       // resolve([1, 2, 3])
+//       reject('Error in delay')
+//     }, time)
+//   })
+//   return promise
+// }
 
-delay(2500)
-  .then((data) => {
-  console.log('Timeout', data)
-  return data.map((x) => x ** 2)
-}).then((data) => {
-  console.log(data)
-}).catch((err) => {
-  console.log(err)
-}).finally(() => {
-  console.log('Finally')
-})
+// delay(2500)
+//   .then((data) => {
+//   console.log('Timeout', data)
+//   return data.map((x) => x ** 2)
+// }).then((data) => {
+//   console.log(data)
+// }).catch((err) => {
+//   console.log(err)
+// }).finally(() => {
+//   console.log('Finally')
+// })
+
+const getData = () => new Promise((resolve) => resolve([1, 2, 3]))
+
+getData().then((array) => console.log(array))
+
+function asyncExample() {
+  
+}
